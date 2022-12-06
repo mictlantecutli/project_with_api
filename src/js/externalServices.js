dcd42790260ec24b9450da908cbccb4fa3801e98
mictlantecutli <moisesparra82@hotmail.com>
2022-12-06 14:56:23 -0600

    getting data

--- src/js/externalServices.js
+++ src/js/externalServices.js
@@ -1,13 +1,17 @@
-const baseURL = 'http://server-nodejs.cit.byui.edu:3000/'
+/*const baseURL = 'http://server-nodejs.cit.byui.edu:3000/'*/
 
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
     return data;
   } else {
-    throw {name:'servicesError', message: data};
+    throw {
+      name: 'servicesError',
+      message: data
+    };
   }
 }
+
 
 export default class ExternalServices {
   constructor() {
@@ -16,24 +20,37 @@ export default class ExternalServices {
 
   }
 
-  getData(category) {
-    return fetch(baseURL + `products/search/${category}`)
-                .then(convertToJson).then((data) => data.Result);
+  getData() {
+    /*return fetch(baseURL + `products/search/${category}`)
+      .then(convertToJson).then((data) => data.Result);*/
+
+      var newsAPIkey = "nrjs2TtoQ78Gp18Z8dfJ2T3hCPaztLnN";
+
+      //Fetch news from NYT api
+      
+      var newsQueryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=astronomy&nasa&outerspace&api-key=" + newsAPIkey;
+      return fetch(newsQueryUrl)
+      .then(convertToJson).then((data) => data.response);
+
+   
     
+
   }
 
-  async findProductById(id) {
+  /*async findProductById(id) {
     //const products = await this.getData();
     //return products.find(function (item) {
     //  return item.Id === id;
     //});
-    return await fetch(baseURL + `product/${id}`).then(convertToJson).then((data)=>data.Result);
+    return await fetch(baseURL + `product/${id}`).then(convertToJson).then((data) => data.Result);
   }
 
-  async checkout(payload){
+
+
+  async checkout(payload) {
     const options = {
       method: 'POST',
-      headers:{
+      headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(payload),
@@ -42,9 +59,9 @@ export default class ExternalServices {
     return await fetch(baseURL + 'checkout/', options).then(convertToJson);
   }
 
-  async loginRequest(user){
+  async loginRequest(user) {
     const options = {
-      method:'POST',
+      method: 'POST',
       headers: {
         'content-Type': 'application/json'
       },
@@ -54,16 +71,16 @@ export default class ExternalServices {
     return response.accessToken;
   }
 
-  async getOrders(token){
+  async getOrders(token) {
     const options = {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${token}`
       }
     }
-    const response= await fetch(baseURL + 'orders', options).then(convertToJson);
+    const response = await fetch(baseURL + 'orders', options).then(convertToJson);
     return response;
   }
-
+*/
 
 }
--- src/js/productList.js
+++ src/js/productList.js
@@ -1,30 +1,58 @@
 import { renderListWithTemplate } from "./utils.js";
 
 export default class ProductList {
-  constructor(category, dataSource, listElement) {
-    this.category = category;
+  constructor(dataSource, listElement) {
+    //this.category = category;
     this.dataSource = dataSource;
     this.listElement = listElement;
   }
 
   async init() {
-    const list = await this.dataSource.getData(this.category);
-    console.log(list);
+    const newsData = await this.dataSource.getData();
+    console.log(newsData);
+
+
+
+    //lenght_news = newsData.response.docs.length;
+    //console.log(lenght_news)
+
+    /*for (var i = 0; i < newsData.response.docs.length; i++) {
+      var newsUrl = newsData.response.docs[i].web_url;
+      var newsTitle = newsData.response.docs[i].abstract;
+    }*/
+
+
+
     //let short = [];
     //short.push(list[0],list[1],list[2],list[3]);
-    this.renderList(list);
-    document.querySelector('.title').innerHTML = this.category;
+    //this.renderList(this.listElement);
+    //document.querySelector('.title').innerHTML = this.category;
+
+   
   }
 
-  renderList(list) {
-    this.listElement.innerHTML = '';
+  renderList(dataSource) {
+
+    for (var i = 0; i < dataSource.response.docs.length; i++) {
+      var newsUrl = dataSource.response.docs[i].web_url;
+      var newsTitle = dataSource.response.docs[i].abstract;
+      //Use tailwindcss group list: https://flowbite.com/docs/components/list-group/
+      //let latestNewsEl = $('<li>');
+      //latestNewsEl.addClass("block py-2 px-4 w-full border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white").append([i+1],". <a href='" + newsUrl + "' target='_blank'>" + newsTitle + "</a>");
+      //latestNewsEl.append($('<br>'));
+      //newsListEl.append(latestNewsEl);
+  }
+    
+    
+    
+    /*this.listElement.innerHTML = '';
     const template = document.getElementById("product-card-template");
     renderListWithTemplate(
       template,
       this.listElement,
       list,
       this.prepareTemplate
-    );
+    );*/
   }
 
   prepareTemplate(template, product) {
