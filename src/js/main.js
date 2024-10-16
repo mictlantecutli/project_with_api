15ae190b610265e0bb1dbdc2f06c103ed19e2214
mictlantecutli <par21078@byui.edu>
2024-10-15 22:06:38 -0600

    changes in the header-footer main view

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,21 @@
 import ExternalServices from "./externalServices.js";
 import ProductList from "./productList.js";
+import { getParams, loadHeaderFooter } from "./utils.js";
 
 /*import {
   loadHeaderFooter
 } from "./utils.js";
 
 loadHeaderFooter();*/
+loadHeaderFooter();
 
 const dataSource = new ExternalServices();
 
 const listEle = document.querySelector(".mission");
 
-
 const listItems = new ProductList(dataSource, listEle);
 
-
 listItems.news_sample();
-
-
 
 //target item in first square
 let change = document.querySelector("#icon1");
@@ -30,7 +28,6 @@ change.addEventListener("mouseover", function () {
 // add event listenr to stop
 change.addEventListener("mouseout", function () {
   change.style.animation = "";
-
 });
 
 let change2 = document.querySelector("#icon2");
@@ -43,5 +40,4 @@ change2.addEventListener("mouseover", function () {
 // add event listenr to stop
 change2.addEventListener("mouseout", function () {
   change2.style.animation = "";
-
 });
